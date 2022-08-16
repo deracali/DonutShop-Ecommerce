cca2c217cf525e4e15eaaac7ef875cd12cf6f9b9
deracali <chideracalistus00@gmail.com>
2022-08-15 23:29:23 -1100

    removed duplicate files

--- src/assets/Components/styled/imgStyled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const Img = styled.img`
-width:${(p) => (p.width ? p.width : "100%")}
-height:${(p) => (p.width ? p.width : "")}
-`;
-
-export default Img;
